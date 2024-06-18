1f48d7da574bc93609f5324d163aa34c55a6cbd5
sameeullasf07 <sameeullasf426@gmail.com>
2024-06-18 18:40:10 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,16 @@
-resource "aws_ec2_availability_zone_group" "example" {
-  group_name    = "us-west-2-lax-1"
-  opt_in_status = "opted-in"
+data "aws_eks_cluster" "example" {
+  name = "example"
+}
+
+output "endpoint" {
+  value = data.aws_eks_cluster.example.endpoint
+}
+
+output "kubeconfig-certificate-authority-data" {
+  value = data.aws_eks_cluster.example.certificate_authority[0].data
+}
+
+# Only available on Kubernetes version 1.13 and 1.14 clusters created or upgraded on or after September 3, 2019.
+output "identity-oidc-issuer" {
+  value = data.aws_eks_cluster.example.identity[0].oidc[0].issuer
 }
